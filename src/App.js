9abbb534ce423e056f2549cba4cce6eb0cc4969f
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-06-01 19:00:52 -0300

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import Calculator from "./components/Calculator";
-import "./App.css";
+import React from 'react';
+import Calculator from './components/Calculator';
+import './App.css';
 
 function App() {
   return <Calculator />;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,48 @@
-import React, { Component } from "react";
-import './Calculator.css'
+import React, { Component } from 'react';
+import './Calculator.css';
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <section className="clac-container">
         <p>0</p>
         <section className="btn-container">
-          <button>AC</button>
-          <button>+/-</button>
-          <button>%</button>
-          <button className="orange">÷</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className="orange">X</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button className="orange">-</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button className="orange">+</button>
-          <button id="extend">0</button>
-          <button>.</button>
-          <button className="orange">=</button>
+          <button type="button">AC</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button" className="orange">
+            ÷
+          </button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button" className="orange">
+            X
+          </button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button" className="orange">
+            -
+          </button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button" className="orange">
+            +
+          </button>
+          <button type="button" id="extend">
+            0
+          </button>
+          <button type="button">.</button>
+          <button type="button" className="orange">
+            =
+          </button>
         </section>
       </section>
     );
