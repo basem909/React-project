4af40511ca4f8ef60762e1f27cf3609a3fe62381
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-06-03 03:06:45 -0300

    Fix eslint errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,145 +4,141 @@ import CalButton from './calButton';
 import calculate from '../logic/calculate';
 
 const Calculator = () => {
- 
-    const [state,setState] = useState({ total: 0, next: null, operation: null });
-    
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
 
   const clickHandler = (e) => {
     const result = calculate(state, e.target.id);
     setState(result);
   };
 
-  
-    return (
-      <section className="clac-container">
-        <p>
-          {state.total}
-          {' '}
-          {state.operation}
-          {' '}
-          {state.next}
-        </p>
-        <section className="btn-container">
-          <CalButton
-            id="AC"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
+  return (
+    <section className="clac-container">
+      <p>
+        {state.total}
+        {' '}
+        {state.operation}
+        {' '}
+        {state.next}
+      </p>
+      <section className="btn-container">
+        <CalButton
+          id="AC"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
 
-          <CalButton
-            id="+/-"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="%"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="÷"
-            type="button"
-            className="btn orange"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="7"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="8"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="9"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="x"
-            type="button"
-            className="btn orange"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="4"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="5"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="6"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="-"
-            type="button"
-            className="btn orange"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="1"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="2"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="3"
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="+"
-            type="button"
-            className="btn orange"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="0"
-            type="button"
-            className="btn extend"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="."
-            type="button"
-            className="btn"
-            onClick={clickHandler}
-          />
-          <CalButton
-            id="="
-            type="button"
-            className="btn orange"
-            onClick={clickHandler}
-          />
-        </section>
+        <CalButton
+          id="+/-"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="%"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="÷"
+          type="button"
+          className="btn orange"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="7"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="8"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="9"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="x"
+          type="button"
+          className="btn orange"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="4"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="5"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="6"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="-"
+          type="button"
+          className="btn orange"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="1"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="2"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="3"
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="+"
+          type="button"
+          className="btn orange"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="0"
+          type="button"
+          className="btn extend"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="."
+          type="button"
+          className="btn"
+          onClick={clickHandler}
+        />
+        <CalButton
+          id="="
+          type="button"
+          className="btn orange"
+          onClick={clickHandler}
+        />
       </section>
-    );
-  
-}
+    </section>
+  );
+};
 
 export default Calculator;
